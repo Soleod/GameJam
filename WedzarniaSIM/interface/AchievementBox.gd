1bd4648a857226dd1c7ffa9aafbd478a15d31ca1
Bartosz Hojnacki <hojnacki.bartosz@gmail.com>
2022-02-27 14:18:33 +0100

    Dodany dym, fix dla lokalizacji achivemntów, większa ryba

--- WedzarniaSIM/interface/AchievementBox.gd
+++ WedzarniaSIM/interface/AchievementBox.gd
@@ -31,22 +31,22 @@ func _on_AchievementButton_pressed():
 
 func _on_AchievementTimer_timeout():
 	if ArchivementManager.modifier_archiv_dict["sledzik_na_raz"] and not mod_achieved["sledzik_na_raz"]:
-		$VBoxContainer/Achievement1.texture = load("res://interface/images/achievement.png")
+		$VBoxContainer/Achievement1.texture = load("res://interface/images/achievements/achievement.png")
 		mod_achieved["sledzik_na_raz"] = true
 	if ArchivementManager.modifier_archiv_dict["great_opor_TUNA_ty"] and not mod_achieved["great_opor_TUNA_ty"]:
-		$VBoxContainer/Achievement2.texture = load("res://interface/images/achievement.png")
+		$VBoxContainer/Achievement2.texture = load("res://interface/images/achievements/achievement.png")
 		mod_achieved["great_opor_TUNA_ty"] = true
 	if ArchivementManager.modifier_archiv_dict["the_production_is_SCALing_up"] and not mod_achieved["the_production_is_SCALing_up"]:
-		$VBoxContainer/Achievement3.texture = load("res://interface/images/achievement.png")
+		$VBoxContainer/Achievement3.texture = load("res://interface/images/achievements/achievement.png")
 		mod_achieved["the_production_is_SCALing_up"] = true
 	if ArchivementManager.fisz_count_archiv_dict["cos_jest_fishy"] and not fisz_achieved["cos_jest_fishy"]:
-		$VBoxContainer/Achievement4.texture = load("res://interface/images/achievement.png")
+		$VBoxContainer/Achievement4.texture = load("res://interface/images/achievements/achievement.png")
 		fisz_achieved["cos_jest_fishy"] = true
 	if ArchivementManager.fisz_count_archiv_dict["what_does_a_BASS_say"] and not fisz_achieved["what_does_a_BASS_say"]:
-		$VBoxContainer/Achievement5.texture = load("res://interface/images/achievement.png")
+		$VBoxContainer/Achievement5.texture = load("res://interface/images/achievements/achievement.png")
 		fisz_achieved["what_does_a_BASS_say"]
 	if ArchivementManager.fisz_count_archiv_dict["dont_make_a_CLOWNFISH"] and not fisz_achieved["dont_make_a_CLOWNFISH"]:
-		$VBoxContainer/Achievement6.texture = load("res://interface/images/achievement.png")
+		$VBoxContainer/Achievement6.texture = load("res://interface/images/achievements/achievement.png")
 		fisz_achieved["dont_make_a_CLOWNFISH"]
 
 
